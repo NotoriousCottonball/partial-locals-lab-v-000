a7206dcf43b5909e3e004c1eabde123ae829f2a8
George  Pianka <gpianka1@gmail.com>
2019-02-14 09:45:44 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,30 +1,10 @@
-class StudentsController < ApplicationController
-  def new
-    @student = Student.new
-  end
-
-  def create
-    @student = Student.new(student_params)
-    if @student.save
-      redirect_to @student
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @student = Student.find(params[:id])
-  end
-
+class ClassroomsController < ApplicationController
   def show
-    @student = Student.find(params[:id])
+    @classroom = Classroom.find(params[:id])
+    # @student = @classroom
   end
 
   def index
-    @students = Student.all
-  end
-
-  def student_params
-    params.require(:student).permit(:name, :birthday, :hometown)
+    @classrooms = Classroom.all
   end
 end
